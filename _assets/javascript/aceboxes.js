771ef2fd1fb62b6fbb993ea283997c1ec6669062
Ace <ace.github@gmail.com>
2014-12-31 16:33:11 -0800

    Initial commit

---
+++ _assets/javascript/aceboxes.js
@@ -0,0 +1,10 @@
+$( "#webdev-button" ).click(function() {
+  $( "#webdev" ).toggle( "slow", function() {
+  });
+});
+
+$( "#author-button" ).click(function() {
+  $( "#author" ).toggle( "slow", function() {
+  });
+});
+
